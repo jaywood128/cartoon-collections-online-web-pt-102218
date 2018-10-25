272e6b71ee8604cfd35016853b2f7cea700e12cf
Johnathon Wood <jaywood128@gmail.com>
2018-10-25 17:08:37 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,8 +19,9 @@ end
 
 def find_the_cheese(cheese)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  if cheese_types.include?(cheese)
+  if cheese_types.include?(cheese) == true
     return cheese_types.first
-
-end
+  else 
+    nil
+  end
 end
